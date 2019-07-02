2361266a869cadc8461456bbd067830b43b0f6d1
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2019-07-02 20:50:23 +0200

    Caesar cipher decryption added. Minor restructuring to avoid code duplication.

--- caesar.py
+++ caesar.py
@@ -1,19 +1,34 @@
 
-ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-FIRST_LETTER = ord(ALPHABET[0])
-NUMBER_OF_LETTERS = len(ALPHABET)
+ENGLISH_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+FIRST_LETTER = ord(ENGLISH_ALPHABET[0])
+NUMBER_OF_LETTERS = len(ENGLISH_ALPHABET)
 
 
 def encrypt(message, key):
     """ Returns an encrypted message using the Caesar Cipher method.
-        Shifts every letter by the amount given by the key.
-        Character not part of the English alphabet are ignored by the cipher and included in the final result,
-        (spaces, numbers, punctuation, other alphabet letters...) """
+        Characters not part of the English alphabet are ignored by the cipher and included in the final result.
+        For example: spaces, numbers, punctuation, other alphabet letters... """
+    return apply(message, key, True)
+
+
+def decrypt(message, key):
+    """ Returns a decrypted message using the Caesar Cipher method.
+        Characters not part of the English alphabet are ignored by the cipher and included in the final result.
+        For example: spaces, numbers, punctuation, other alphabet letters... """
+    return apply(message, key, False)
+
+
+def apply(message, key, encryption):
+    """ Shifts every letter by the amount given by the key adding for encryption and subtracting for decryption """
     encrypted = ''
     for letter in message:  # For every letter in the message
-        if letter in ALPHABET:  # Only applies the cipher to letters, other characters are not encrypted
+        if letter in ENGLISH_ALPHABET:  # Only applies the cipher to letters, other characters are not encrypted
             number = ord(letter) - FIRST_LETTER  # Converts the letter into its equivalent number (0 - 25)
-            number = (number + key) % NUMBER_OF_LETTERS  # Adds the key shifting the letter by letter
+            if encryption:  # Shifts the letter by the amount given by the key
+                number = number + key  # Adding the shift for encryption
+            else:
+                number = number - key  # Subtracting the shift for decryption
+            number = number % NUMBER_OF_LETTERS  # Keep the number between 0 and 25 by using the modulus operator
             letter = chr(number + FIRST_LETTER)  # Converts the number back to a letter (97 - 122)
         encrypted += letter  # Puts the letter at the end of the encrypted result message
     return encrypted
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import caesar
 
+print('1. Encryption')
+print('2. Decryption')
+choice = input('Your choice: ')
 message = input('Message: ')
-secret = caesar.encrypt(message.lower(), -1)
-print(secret)
+if choice == '1':
+    secret = caesar.encrypt(message.lower(), -1)
+    print(secret)
+elif choice == '2':
+    secret = caesar.decrypt(message.lower(), -1)
+    print(secret)
+else:
+    print('Invalid choice')

c8195f18f9bafe2506562796de62563d42dc8b86
USUARIO-PC\USUARIO <jrinconada@gmail.com>
2019-07-04 19:17:51 +0200

    Help message formatting changed

--- main.py
+++ main.py
@@ -5,52 +5,50 @@ import analysis
 
 
 def usage():
-    manual = 'Usage: main.py [cipher] [encrypt/decrypt] [key] [message] [-i in.txt] [-o out.txt]'
-    manual += 'Ciphers: caesar vigenere'
-    manual += 'Examples:'
-    manual += '\tmain.py caesar encrypt 13 hello'
-    manual += '\tmain.py c e 13 hello'
-    manual += '\tmain.py v e thekey this is a longer message'
-    manual += '\tmain.py caesar decrypt -1 hal'
-    manual += '\tmain.py caesar encrypt 13 -i letter.txt -o secret.txt'
-    return manual
+    return """
+    Usage: main.py [cipher] [encrypt/decrypt] [key] [message] [-i in.txt] [-o out.txt]
+    Ciphers: caesar vigenere
+    Examples:
+        main.py caesar encrypt 13 hello
+        main.py c e 13 hello
+        main.py v e thekey this is a longer message
+        main.py caesar decrypt -1 hal
+        main.py caesar encrypt 13 -i letter.txt -o secret.txt"""
 
 
-analysis.show()
+if len(sys.argv) < 5:
+    print(usage())  # Invalid number of arguments
+    exit(-1)
 
-#
-# if len(sys.argv) < 5:
-#     print(usage())
-#     exit(-1)
-#
-# method = sys.argv[1][0]
-# choice = sys.argv[2][0]
-# if '-i' in sys.argv:  # Message from file
-#     message = storage.read(sys.argv[5])
-# else:  # Message from arguments
-#     message = ' '.join(sys.argv[4:])
-#
-# if method == 'c':  # Caesar cipher
-#     key = eval(sys.argv[3])  # In the Caesar cipher the key is an integer number
-#     if choice == 'e':  # Encryption
-#         result = cipher.caesar(message.lower(), key)
-#     elif choice == 'd':  # Decryption
-#         result = cipher.caesar(message.lower(), key, False)
-#     else:
-#         result = usage()
-# elif method == 'v':  # Vigenere cipher
-#     key = sys.argv[3]  # In Vigenere cipher the key is a word
-#     if choice == 'e':  # Encryption
-#         result = cipher.vigenere(message.lower(), key.lower())
-#     elif choice == 'd':  # Decryption
-#         result = cipher.vigenere(message.lower(), key.lower(), False)
-#     else:
-#         result = usage()
-# else:
-#     result = usage()
-#
-# if '-o' in sys.argv:  # Result to file
-#     i = sys.argv.index('-o')
-#     storage.write(sys.argv[i + 1], result)
-# else:  # Result to console
-#     print(result)
+method = sys.argv[1][0]  # caesar or vigenere
+choice = sys.argv[2][0]  # encryption or decryption
+
+if '-i' in sys.argv:  # Message from file
+    message = storage.read(sys.argv[5])
+else:  # Message from arguments
+    message = ' '.join(sys.argv[4:])
+
+if method == 'c':  # Caesar cipher
+    key = eval(sys.argv[3])  # In the Caesar cipher the key is an integer number
+    if choice == 'e':  # Encryption
+        result = cipher.caesar(message.lower(), key)
+    elif choice == 'd':  # Decryption
+        result = cipher.caesar(message.lower(), key, False)
+    else:
+        result = usage()  # Invalid argument
+elif method == 'v':  # Vigenere cipher
+    key = sys.argv[3]  # In Vigenere cipher the key is a word
+    if choice == 'e':  # Encryption
+        result = cipher.vigenere(message.lower(), key.lower())
+    elif choice == 'd':  # Decryption
+        result = cipher.vigenere(message.lower(), key.lower(), False)
+    else:
+        result = usage()  # Invalid argument
+else:
+    result = usage()  # Invalid argument
+
+if '-o' in sys.argv:  # Result to file
+    i = sys.argv.index('-o')
+    storage.write(sys.argv[i + 1], result)
+else:  # Result to console
+    print(result)
